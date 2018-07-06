113de855e6b4833404baf7d9916d3c2afb767cab
Abiola Adebanjo <abiola.adebanjo@interswitchgroup.com>
2018-07-06 10:45:13 +0100

    Add more test cases and refactor

--- hello.go
+++ hello.go
@@ -1,5 +1,13 @@
+//following tutorial on https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world
 package main
 
-func Hello() string {
-	return "Hello, testing"
+const helloPrefix = "Hello, "
+
+//Hello : function to learn unit testing
+func Hello(name string) string {
+	if name == "" {
+		name = "Testing"
+	}
+
+	return helloPrefix + name
 }
--- hello_test.go
+++ hello_test.go
@@ -1,12 +1,29 @@
+//following tutorial on https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world
 package main
 
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, testing"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("hello with non-empty argument", func(t *testing.T) {
+		got := Hello("Abiola")
+		want := "Hello, Abiola"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("hello with empty argument", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Testing"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
