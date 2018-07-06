39d6e91b14591f26ee45d4e009413d23b0af9191
Abiola Adebanjo <abiola.adebanjo@interswitchgroup.com>
2018-07-06 12:57:32 +0100

    Modify test to accept count as parameter and fix code appropriately

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,9 @@
 //Package iteration based on this tutorial https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/iteration
 package iteration
 
-const repeatCount = 5
-
 //Repeat : accepts a character and repeats it 5 times
-func Repeat(character string) (repeated string) {
-	for i := 0; i < repeatCount; i++ {
+func Repeat(character string, count int) (repeated string) {
+	for i := 0; i < count; i++ {
 		repeated += character
 	}
 
