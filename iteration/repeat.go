5a185f006459219776f8dfbc342ca34f2dcf32ea
Abiola Adebanjo <abiola.adebanjo@interswitchgroup.com>
2018-07-06 13:04:02 +0100

    ExampleRepeat to document the function

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,7 +1,7 @@
 //Package iteration based on this tutorial https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/iteration
 package iteration
 
-//Repeat : accepts a character and repeats it 5 times
+//Repeat : accepts a character and repeats it "count" times
 func Repeat(character string, count int) (repeated string) {
 	for i := 0; i < count; i++ {
 		repeated += character
